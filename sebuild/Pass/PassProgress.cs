a5f4f877a46411d512273f5d1426ebec99b6bbad
Bendi11 <bkliebmann@gmail.com>
2024-01-19 12:14:21 -0800

    Began adding progress bar display for some compilation passes

--- sebuild/Pass/PassProgress.cs
+++ sebuild/Pass/PassProgress.cs
@@ -4,49 +4,98 @@ using System.Diagnostics;
 
 namespace SeBuild;
 
+/// <summary>
+/// Implementation of <c>IProgress</c> intended for use by <c>CompilationPass</c> instances.
+/// Writes formatted messages to the console while ticking a console spinner. When disposed, 
+/// displays a message including recorded time of the operation
+/// </summary>
 public class PassProgress: IProgress<int>, IDisposable {
-    string _name;
-    public string? Message = null;
-    int _total = 0;
+    string _tag;
+    Mode _mode;
+    
+    //The actual message with additional formatting added
+    string? _message;
+    int _progress, _total;
     Stopwatch _stopWatch;
-    bool _useNumbers;
     CancellationTokenSource? _tick = null;
+    
+    /// <summary>
+    /// Progress display mode to be selected based on what information is available at the time of progress creation:
+    /// A progress bar if total number of items is known, a number if operations can be counted, or nothing
+    /// </summary>
+    public enum Mode {
+        NoProgress,
+        Count,
+        Bar,
+    }
 
-    public PassProgress(string name, bool useNumbers = true) {
-        _name = name;
+    /// <summary>
+    /// The formatted message that will is currently displayed on the console
+    /// </summary>
+    public string? Message {
+        get => _message;
+        set => _message = value ?? $" - {value}";
+    }
+    
+    /// <summary>
+    /// Create a new progress reporter with a <paramref name="name"/> to be displayed alongside a message,
+    /// and a <paramref name="mode"/> of displaying progress
+    /// </summary>
+    /// <param name="mode">Mode to display progress on the console</param>
+    /// <param name="name">Title of the ongoing process</param>
+    public PassProgress(string name, Mode mode = Mode.Count) {
+        _tag = name;
         _stopWatch = new Stopwatch();
         _stopWatch.Start();
-        _useNumbers = useNumbers;
-        if(!_useNumbers) {
-            _tick = new CancellationTokenSource();
-            Task.Run(
-                    async () => {
-                    for(;;) {
-                        await Task.Delay(100);
-                        Report(1);
-                    }
-                },
-                _tick.Token
-            );
+        _progress = 0;
+        _total = 1;
+        _message = null;
+    }
+    
+    /// <summary>
+    /// Add a progress bar displaying the current number of items out of <paramref name="totalItems"/>
+    /// </summary>
+    /// <param name="totalItems">Must be greater than 0</param>
+    /// <exception cref="ArgumentException">When <paramref name="totalItems"/> is less than or equal to 0</exception>
+    public void AddBar(int totalItems) {
+        if(totalItems <= 0) {
+            throw new ArgumentException($"Total items must be greater than 0");
+        }
+
+        _total = totalItems;
+        _mode = Mode.Bar;
+    }
+    
+    ///<summary>A string that can be used to clear a line of the console</summary>
+    static readonly string CLEAR = new string(' ', Console.WindowWidth - 1);
+    
+    /// <summary>
+    /// Report <paramref name="items"/> number of items complete, and display the updated progress information
+    /// </summary>
+    public void Report(int items) {
+        _progress += items;
+        Console.CursorVisible = false;
+        ClearLine();
+
+
+        switch(_mode) {
+            case Mode.NoProgress:
+                Console.Write($"⟳ {_tag} {Message}\r");
+            break;
+
+            case Mode.Count:
+                Console.Write($"⟳ {_tag} [{_progress}] {Message}\r");
+            break;
+
+            case Mode.Bar:
+                Console.Write($"⟳ {_tag} {Message}\r");
+                Console.Write('\n');
+                Console.Write($"PROGRESS");
+            break;
         }
     }
 
-    static readonly char[] TICKER = {'▉', '▊', '▋', '▌', '▍', '▎', '▏', '▎', '▍', '▌', '▋', '▊', '▉'};
-    static readonly string CLEAR = new string(' ', Console.WindowWidth - 1);
 
-    void ClearLine() {
-        Console.SetCursorPosition(0, Console.GetCursorPosition().Top);
-        Console.Write(CLEAR);
-        Console.SetCursorPosition(0, Console.GetCursorPosition().Top);
-    }
-
-    public void Report(int value) {
-        _total += value;
-        Console.CursorVisible = false;
-        ClearLine();
-        var total = _useNumbers ? $"[{_total:0,0}]" : "";
-        Console.Write($"⟳ {_name}: {total} {TICKER[_total % TICKER.Count()]}{(Message is null ? "" : $" - {Message}")}\r");
-    }
 
     public void Dispose() {
         _stopWatch.Stop();
@@ -58,9 +107,15 @@ public class PassProgress: IProgress<int>, IDisposable {
 
         ClearLine();
         var total = _useNumbers ? $"- {_total}" : "";
-        Console.WriteLine($"✓ {_name} {total} ({_stopWatch.Elapsed.TotalSeconds:0.000})");
+        Console.WriteLine($"✓ {_tag} {total} ({_stopWatch.Elapsed.TotalSeconds:0.000})");
 
         Console.CursorVisible = true;
         Console.ForegroundColor = old;
+    }
+
+    private void ClearLine() {
+        Console.SetCursorPosition(0, Console.GetCursorPosition().Top);
+        Console.Write(CLEAR);
+        Console.SetCursorPosition(0, Console.GetCursorPosition().Top);
     }
 }
